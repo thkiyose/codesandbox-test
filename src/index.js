71a5e12f80795c69a83293ac8d78c0ebb84ea0a6
thkiyose <59986837+thkiyose@users.noreply.github.com>
2022-05-13 13:37:57 +0900

    commit test

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ document.getElementById("app").innerHTML = `
 <p>
 あああ
 </p>
+<p>テスト</p>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
